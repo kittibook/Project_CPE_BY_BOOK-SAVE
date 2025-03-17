bc9f7dc5392dc16eb260d37721646fc73d71206a
BXOK <kittibook@icloud.com>
2025-03-17 16:25:06 +0700

    del

--- api/src/Routes/auth.ts
+++
@@ -1,5 +0,0 @@
-import { Elysia } from "elysia";
-import authController from "../Controllers/auth.controller";
-
-export const auth = new Elysia({ prefix : "/auth"})
-  .post('/login', authController.login)
--- api/src/Routes/game.ts
+++
@@ -1,4 +0,0 @@
-import { Elysia } from "elysia";
-
-export const gamne = new Elysia({ prefix : "/game"})
-  .get("/", () => "Hello Game")
